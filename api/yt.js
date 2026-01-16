f0eb87cfc8b427aee3c171f798e70f66a6068c02
JEAN-S DEV <nguessanjeanstephane87@gmail.com>
2026-01-16 17:06:55 +0000

    Implement YouTube video search and download API

--- api/yt.js
+++ api/yt.js
@@ -1,1 +1,28 @@
+import ytdl from 'ytdl-core'
+import ytSearch from 'yt-search'
 
+export default async (req, res) => {
+  try {
+    const query = req.query.query
+    if (!query) return res.json({ status: false })
+
+    const search = await ytSearch(query)
+    const video = search.videos[0]
+
+    const info = await ytdl.getInfo(video.url)
+    const format = ytdl.chooseFormat(info.formats, { quality: '18' })
+
+    res.json({
+      status: true,
+      result: {
+        title: video.title,
+        duration: video.timestamp,
+        views: video.views,
+        thumbnail: video.thumbnail,
+        download_url: format.url
+      }
+    })
+  } catch (e) {
+    res.json({ status: false, error: e.message })
+  }
+}
