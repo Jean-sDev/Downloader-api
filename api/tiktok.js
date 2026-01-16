5f8820c8c635dafd285f2cb761fe2684887cdea1
JEAN-S DEV <nguessanjeanstephane87@gmail.com>
2026-01-16 17:10:48 +0000

    Add TikTok API handler to fetch video details

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,1 +1,22 @@
+import axios from 'axios'
 
+export default async (req, res) => {
+  try {
+    const url = req.query.url
+    if (!url) return res.json({ status: false })
+
+    const api = `https://tikwm.com/api/?url=${encodeURIComponent(url)}`
+    const { data } = await axios.get(api)
+
+    res.json({
+      status: true,
+      result: {
+        video: data.data.play,
+        music: data.data.music,
+        author: data.data.author.nickname
+      }
+    })
+  } catch (e) {
+    res.json({ status: false, error: e.message })
+  }
+}
